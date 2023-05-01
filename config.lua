7751fd4897eabf068707a4b6380ecaf98346b023
Camilo Torres <camilotorresf@gmail.com>
2023-05-01 11:14:31 -0400

    Remove prettier and eslint.
    Makes things slow.

--- config.lua
+++ config.lua
@@ -11,7 +11,7 @@ vim.opt.relativenumber = true
 lvim.log.level = "info"
 lvim.format_on_save = {
   enabled = true,
-  pattern = "*.lua,*.ts,*.tsx,*.js",
+  pattern = "*.lua",
   timeout = 1000,
 }
 -- to disable icons and use a minimalist setup, uncomment the following
@@ -100,25 +100,25 @@ lvim.builtin.treesitter.auto_install = true
 -- end
 
 -- -- linters, formatters and code actions <https://www.lunarvim.org/docs/languages#lintingformatting>
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  --   { command = "stylua" },
-  {
-    command = "prettier",
-    extra_args = { "--print-width", "100" },
-    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "vue", "css", "scss", "less", "html",
-      "json", "jsonc", "yaml", "graphql", "handlebars" },
-  },
-}
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  --   { command = "flake8", filetypes = { "python" } },
-  --   {
-  --     command = "shellcheck",
-  --     args = { "--severity", "warning" },
-  --   },
-  { command = "eslint", filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" } }
-}
+-- local formatters = require "lvim.lsp.null-ls.formatters"
+-- formatters.setup {
+--   { command = "stylua" },
+--   {
+--     command = "prettier",
+--     extra_args = { "--print-width", "100" },
+--     filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "vue", "css", "scss", "less", "html",
+--       "json", "jsonc", "yaml", "graphql", "handlebars" },
+--   },
+-- }
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+--   { command = "flake8", filetypes = { "python" } },
+--   {
+--     command = "shellcheck",
+--     args = { "--severity", "warning" },
+--   },
+--   { command = "eslint", filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" } }
+-- }
 -- local code_actions = require "lvim.lsp.null-ls.code_actions"
 -- code_actions.setup {
 --   {
